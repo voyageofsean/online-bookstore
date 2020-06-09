f83668602bb58aec461817178d5a70418da84dac
asebee <56385753+asebee@users.noreply.github.com>
2020-06-09 21:01:41 +0800

    Add files via upload

--- mysite/online_bookstore/urls.py
+++ mysite/online_bookstore/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/',views.login),
+    path('register/',views.register),
+    path('search/',views.search),
+    #不知道为什么没匹配成 有个常用匹配 https://www.jianshu.com/p/257fafc217df
+    path(r'^bookInformation/(?P<isbn>[-\w]+)/$',views.bookInformation),
+    path('showBook/',views.showBook),
 ]
--- mysite/online_bookstore/views.py
+++ mysite/online_bookstore/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from .models import Book,User,ShoppingCart,Order
+from random import sample
 # Create your views here.
 import xlrd
 
@@ -22,3 +24,76 @@ def insert_book():
             PublishDate = sheet.cell(i,5),
             ShelfDate = sheet.cell(i,6),
             BookInformation = sheet.cell(i,7))
+        
+#注册功能
+def register(request):
+    # 定义一个错误提示为空
+    error_name = ''
+    if request.method=='POST':
+        user = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        user_list = User.objects.filter(UserName=user)
+        if user_list :
+            # 注册失败
+            error_name = '%s用户名已经存在了' % user
+            # 返回到注册页面，并且把错误信息报出来
+            return  render(request,'register.html',{'error_name':error_name})
+        else:
+            # 数据保存在数据库中，并返回到登录页面
+            user = User.objects.create(UserName=user,
+                                       UserKey=password,
+                                       UserAddress=email)
+            user.save()
+            return redirect('/online_bookstore/login/')
+    
+    return render(request,'register.html')
+
+#登录功能
+def login(request):
+    # 定一个为空的错误接收
+    error_msg = ''
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        # 判断数据库中有没有账号密码
+        ret = User.objects.filter(UserName=username, UserKey=password)
+        if ret:
+            # 登录到主界面
+            return redirect('/online_bookstore/')
+        else:
+            # 登录失败
+            error_msg = '用户名或密码错误，请重新输入！'
+    return render(request, 'login.html', {'error_msg':error_msg})
+
+#书籍查询功能
+def search(request):
+    error_msg = ''
+    if request.method == 'POST':
+        query=request.POST.get('q')
+        #查询所有可能结果
+        book_list = (Book.objects.filter(ISBN__icontains=query)|Book.objects.filter(BookName__icontains=query)|Book.objects.filter(BookAuthor__icontains=query)).distinct()
+        if not book_list:
+            error_msg = '没找到所需的书籍'
+        return render(request, 'search.html', {'error_msg': error_msg,'book_list': book_list})            
+    return render(request, 'search.html', {'error_msg': error_msg})
+
+#书籍详细信息功能
+def bookInformation(request,isbn):
+    book=Book.objects.filter(ISBN=isbn)
+    return render(request, 'bookInformation.html', {'book':book})
+
+#书籍展示功能 随机推荐k本书籍
+def showBook(request):
+    ####选择随机推荐的书籍数####
+    k=1
+    ###########################
+    book_list=Book.objects.all()
+    count=book_list.count()
+    rand_ids=sample(range(0,count),k)
+    selected_list=[]
+    
+    for i in rand_ids:
+        selected_list.append(book_list[i])
+        
+    return render(request, 'showBook.html', {'books':selected_list})
